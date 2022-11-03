25ea4fb5019d36b7e33b336ba7a555daecac9808
Haci11 <haci123123@hotmail.com>
2022-11-03 10:11:58 +0100

    Javascript completed for first project

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
 window.onload = function () {
-  console.log("Test");
+  const panels = document.querySelectorAll(".panel");
+
+  panels.forEach((panel) =>
+    panel.addEventListener("click", () => {
+      removeActiveClasses();
+      panel.classList.add("active");
+      console.log("b");
+    })
+  );
+
+  function removeActiveClasses() {
+    panels.forEach((panel) => {
+      panel.classList.remove("active");
+    });
+  }
 };
